383476fdb826686d6aaff88a02199190fd82f438
A-Genov <80156452+A-Genov@users.noreply.github.com>
2021-03-21 15:06:48 +0200

    Fix display of courts

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -11,20 +11,20 @@ const Main = ({
                 <h1 className="main-heading">Wanna book the perfect court for your game?</h1>
                 <button className="main-button">Login</button>
             </section>
-            <section className="court-container">
-                <ul className="court-list">
-                    {courts.map(x =>
-                        <Court
-                            key={x.id}
-                            id={x.id}
-                            name={x.name}
-                            address={x.address}
-                            image={x.image}
-                            price={x.price}
-                        />
-                    )}
-                </ul>
-            </section>
+
+            <ul className="court-list">
+                {courts.map(x =>
+                    <Court
+                        key={x.id}
+                        id={x.id}
+                        name={x.name}
+                        address={x.address}
+                        image={x.image}
+                        price={x.price}
+                    />
+                )}
+            </ul>
+
 
         </main>
     );
